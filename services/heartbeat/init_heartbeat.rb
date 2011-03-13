78af6a037a080a65569285e373c9424c7b28d201
Erik Hollensbe <erik@hollensbe.org>
2011-03-12 19:39:50 -0500

    * Updated heartbeat to have a guesstimated running result

--- services/heartbeat/init_heartbeat.rb
+++ services/heartbeat/init_heartbeat.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'sqlite3'
+require 'fileutils'
 
 SQL = [ 
-  %q[create table heartbeat (ip varchar primary key, gem varchar)]
+  %q[create table heartbeat (ip varchar primary key, gem varchar, running boolean)]
 ]
 
+FileUtils.rm_f 'heartbeat.db'
 db = SQLite3::Database.new 'heartbeat.db'
 
 SQL.each do |sql|
