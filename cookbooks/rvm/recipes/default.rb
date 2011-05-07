9f241c997d165e738ae85c205885611a63b62b49
Erik Hollensbe <erik@hollensbe.org>
2011-05-07 13:08:48 -0400

    Got past RVM, pretty hacky shit though.

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -13,40 +13,33 @@ include_recipe "build-essential"
 %w(libreadline5-dev zlib1g-dev libssl-dev libxml2-dev libxslt1-dev).each do |pkg|
   package pkg
 end
- 
+
+bash "fixing ubuntu system-wide bashrc" do
+  user "root"
+  code %q[grep -v '[ -z "$PS1" ] && return' /etc/bash.bashrc >/tmp/bash.bashrc && cp /tmp/bash.bashrc /etc/bash.bashrc]
+end
+
 bash "installing system-wide RVM stable" do
   user "root"
-  code "bash < <( curl -s https://rvm.beginrescueend.com/install/rvm )"
-  not_if "which rvm"
+  code "bash < <( curl -s https://rvm.beginrescueend.com/install/rvm )" 
+  not_if "test -d /usr/local/rvm"
 end
 
 bash "upgrading to RVM head" do
   user "root"
-  code "rvm update --head ; rvm reload"
+  code "/usr/local/rvm/bin/rvm update --head ; /usr/local/rvm/bin/rvm reload"
   only_if { node[:rvm][:version] == :head }
   only_if { node[:rvm][:track_updates] }
 end
 
 bash "upgrading RVM stable" do
   user "root"
-  code "rvm update ; rvm reload"
+  code "/usr/local/rvm/bin/rvm update ; /usr/local/rvm/bin/rvm reload"
   only_if { node[:rvm][:track_updates] }
-end
-
-cookbook_file "/etc/profile.d/rvm.sh" do
-  owner "root"
-  group "root"
-  mode 0755
 end
 
 cookbook_file "/usr/local/bin/rvm-gem.sh" do
   owner "root"
   group "root"
   mode 0755
-end
-
-bash "fixing up bashrc for RVM" do
-  user "root"
-  code '(echo ". /etc/profile.d/rvm.sh"; cat /etc/bash.bashrc) > /tmp/bash.bashrc; mv /tmp/bash.bashrc /etc/bash.bashrc'
-  not_if 'head -1 /etc/bash.bashrc | grep -q rvm'
 end
