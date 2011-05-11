474db10efa425f8f8f0ba5881ff5dea7004cccab
Erik Hollensbe <erik@hollensbe.org>
2011-05-11 07:55:55 -0400

    Even more cleanups

--- cookbooks/gt-services/recipes/gem-sniffer.rb
+++ cookbooks/gt-services/recipes/gem-sniffer.rb
@@ -1,15 +1,5 @@
 include_recipe "gt-services::default"
 
-#
-# refactor into a lib
-#
-
-def install_gem gem 
-  gem_package gem do
-    gem_binary "/usr/local/bin/rvm-gem.sh"
-  end
-end
-
 bash 'copying gem-sniffer code' do
   user 'root'
   code 'cp -R /test/smoking-chef/services/gem-sniffer /root/gem-sniffer && chmod a+x /root/gem-sniffer'
