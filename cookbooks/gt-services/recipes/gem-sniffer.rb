5b075ea66b56b87c4ee5efac6c4b256779b7e752
Erik Hollensbe <erik@hollensbe.org>
2011-05-11 07:55:34 -0400

    Few cleanups for gem-sniffer

--- cookbooks/gt-services/recipes/gem-sniffer.rb
+++ cookbooks/gt-services/recipes/gem-sniffer.rb
@@ -10,14 +10,6 @@ def install_gem gem
   end
 end
 
-%w[sqlite3 libsqlite3-dev].each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
-install_gem 'sqlite3'
-
 bash 'copying gem-sniffer code' do
   user 'root'
   code 'cp -R /test/smoking-chef/services/gem-sniffer /root/gem-sniffer && chmod a+x /root/gem-sniffer'
